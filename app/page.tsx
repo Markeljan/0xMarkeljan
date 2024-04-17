c176a2926895f2eb6b0575f2a221fb465d1dfd8e
Markeljan Sokoli <Markeljan19@gmail.com>
2024-04-17 18:09:58 -0400

    static export

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center space-y-4">
+        <p className="text-4xl">soko | markeljan</p>
+      </div>
+    </main>
+  );
+}
--- components/changeCursor.js
+++
@@ -1,3 +0,0 @@
-export default function changeCursor(cursorName) {
-  document.body.style.cursor = `url(/images/${cursorName}.svg) 32 32, default`;
-}
